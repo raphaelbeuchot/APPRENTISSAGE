a5b66cb8a4edb8ed754b598ea76e070ddf7bce46
raphaelbeuchot <raphaelbeuchot@gmail.com>
2026-03-29 15:48:49 +0200

    Revert "bomb"
    This reverts commit 3b191db43329f9d46374e41ca9f91ab67418c902.

--- Assets/Project/Scripts/Cameras/PlayerVictoryScale.cs
+++ Assets/Project/Scripts/Cameras/PlayerVictoryScale.cs
@@ -1,48 +1,48 @@
 using UnityEngine;
 using System.Collections;
 using System.Collections.Generic;
-
 public class PlayerVictoryScale : MonoBehaviour
 {
     [SerializeField] private SkinnedMeshRenderer playerMeshRenderer;
     [SerializeField] private Material victoryMaterial;
+    [SerializeField] private float scaleSpeed = 10f;
+    [SerializeField] private float targetScale = 100f;
+    [SerializeField] private float moveSpeed = 0f;
     [SerializeField] private int waveCount = 10;
-    [SerializeField] private float delayBetweenWaves = 0.8f;
+    [SerializeField] private float delayBetweenWaves = 0.5f;
     [SerializeField] private Color[] colors = new Color[10];
-    [SerializeField] private GameUIManager gameUIManager;
-
     private Mesh bakedMesh;
     private List<GameObject> ghosts = new List<GameObject>();
     private Vector3 cachedSpawnPos;
-
     public void TriggerScale()
     {
-        if (gameUIManager != null) gameUIManager.HideGameplayBars();
         bakedMesh = new Mesh();
         playerMeshRenderer.BakeMesh(bakedMesh);
         cachedSpawnPos = playerMeshRenderer.transform.position;
         playerMeshRenderer.enabled = false;
-        StartCoroutine(WaveCoroutine());
+        SpawnGhost(0);
+        if (waveCount > 1)
+            StartCoroutine(WaveCoroutine());
     }
-
     private IEnumerator WaveCoroutine()
     {
-        for (int i = 0; i < waveCount; i++)
+        for (int i = 1; i < waveCount; i++)
         {
-            SpawnGhost(i);
             yield return new WaitForSecondsRealtime(delayBetweenWaves);
+            SpawnGhost(i);
         }
     }
-
     private void SpawnGhost(int index)
     {
+        // Centre du mesh bake
         Vector3 meshCenter = bakedMesh.bounds.center;
-        float scale = Mathf.Pow(2f, index);
 
+        // Parent GO au centre du mesh
         GameObject ghost = new GameObject("VictoryGhost_" + index);
         ghost.transform.position = cachedSpawnPos + meshCenter;
-        ghost.transform.localScale = new Vector3(scale, scale, scale);
+        ghost.transform.localScale = Vector3.one;
 
+        // Child GO avec le mesh, offset inverse du centre
         GameObject meshGO = new GameObject("Mesh");
         meshGO.transform.SetParent(ghost.transform);
         meshGO.transform.localPosition = -meshCenter;
@@ -52,12 +52,9 @@ public class PlayerVictoryScale : MonoBehaviour
         MeshFilter mf = meshGO.AddComponent<MeshFilter>();
         mf.mesh = bakedMesh;
         MeshRenderer mr = meshGO.AddComponent<MeshRenderer>();
-
         Material instanceMat = new Material(victoryMaterial);
-        // Ghost 0 devant (renderQueue le plus haut), ghost N derriere
-        instanceMat.renderQueue = victoryMaterial.renderQueue + (waveCount - 1 - index);
+        instanceMat.renderQueue = victoryMaterial.renderQueue + index;
         mr.material = instanceMat;
-
         MaterialPropertyBlock mpb = new MaterialPropertyBlock();
         Color color = (colors != null && index < colors.Length) ? colors[index] : Color.white;
         mpb.SetColor("_Color", color);
@@ -65,5 +62,29 @@ public class PlayerVictoryScale : MonoBehaviour
 
         ghost.AddComponent<VictoryGhostBillboard>();
         ghosts.Add(ghost);
+        StartCoroutine(ScaleCoroutine(ghost));
+    }
+    private IEnumerator ScaleCoroutine(GameObject ghost)
+    {
+        float elapsed = 0f;
+        Vector3 startPos = ghost.transform.position;
+        Vector3 screenCenter = Camera.main.ViewportToWorldPoint(
+            new Vector3(0.5f, 0.5f, Camera.main.WorldToViewportPoint(startPos).z)
+        );
+        while (ghost.transform.localScale.x < targetScale)
+        {
+            float scale = ghost.transform.localScale.x + scaleSpeed * Time.unscaledDeltaTime;
+            scale = Mathf.Min(scale, targetScale);
+            ghost.transform.localScale = new Vector3(scale, scale, scale);
+            ghost.transform.position = Vector3.MoveTowards(
+                ghost.transform.position,
+                screenCenter,
+                moveSpeed * Time.unscaledDeltaTime
+            );
+            yield return new WaitForSecondsRealtime(0f);
+        }
+        Destroy(ghost);
+        ghosts.Remove(ghost);
+       
     }
 }
--- Assets/Project/Scripts/Enemies/Attacks/ExplosionVisual.cs
+++ Assets/Project/Scripts/Enemies/Attacks/ExplosionVisual.cs
@@ -1,84 +1,42 @@
 using UnityEngine;
 using System.Collections;
-using System.Collections.Generic;
 
 public class ExplosionVisual : MonoBehaviour
 {
-    public enum EffectOrientation { None, FlatXZ, Billboard }
-
-    [System.Serializable]
-    public class ExplosionEffect
-    {
-        public bool enabled = true;
-        public GameObject prefab;
-        public EffectOrientation orientation = EffectOrientation.None;
-        public float spawnHeightOffset = 0f;
-    }
-
-    [Header("Effets")]
-    [SerializeField] private List<ExplosionEffect> effects = new List<ExplosionEffect>();
-
-    [Header("Legacy Sphere")]
-    [SerializeField] private bool useLegacySphere = true;
-    [SerializeField] private float legacyStartRadius = 0.25f;
-    [SerializeField] private float legacyTargetRadius = 4f;
-    [SerializeField] private float legacyDuration = 0.4f;
-    [SerializeField] private Material legacyMaterial;
-
     private Vector3 spawnPosition;
 
-    public void Play(Vector3 position)
+    public void Play(float startRadius, float targetRadius, float duration, Material mat, float ringSpawnHeight, Vector3 position, GameObject ringPrefab)
     {
         spawnPosition = position;
-
-        foreach (ExplosionEffect effect in effects)
+        StartCoroutine(ExpandCoroutine(startRadius, targetRadius, duration, mat));
+        if (ringPrefab != null)
         {
-            if (!effect.enabled || effect.prefab == null) continue;
-
-            Vector3 pos = new Vector3(spawnPosition.x, spawnPosition.y + effect.spawnHeightOffset, spawnPosition.z);
-            Quaternion rot = Quaternion.identity;
-
-            if (effect.orientation == EffectOrientation.FlatXZ)
-                rot = Quaternion.Euler(90f, 0f, 0f);
-
-            GameObject go = Instantiate(effect.prefab, pos, rot);
-
-            if (effect.orientation == EffectOrientation.Billboard)
-            {
-                if (go.GetComponent<BillboardRenderer>() == null)
-                    go.AddComponent<BillboardRenderer>();
-            }
+            Vector3 pos = new Vector3(spawnPosition.x, ringSpawnHeight, spawnPosition.z);
+            Instantiate(ringPrefab, pos, Quaternion.Euler(90f, 0f, 0f));
         }
-
-        if (useLegacySphere)
-            StartCoroutine(ExpandCoroutine());
     }
 
-    IEnumerator ExpandCoroutine()
+    IEnumerator ExpandCoroutine(float startRadius, float targetRadius, float duration, Material mat)
     {
-        float targetDiameter = legacyTargetRadius * 2f;
-        float startDiameter = legacyStartRadius * 2f;
-
+        float targetDiameter = targetRadius * 2f;
+        float startDiameter = startRadius * 2f;
         GameObject sphere = GameObject.CreatePrimitive(PrimitiveType.Sphere);
         Destroy(sphere.GetComponent<Collider>());
         sphere.transform.SetParent(transform);
         sphere.transform.localPosition = Vector3.zero;
         sphere.transform.localScale = Vector3.one * startDiameter;
-
         Renderer rend = sphere.GetComponent<Renderer>();
-        if (legacyMaterial != null)
-            rend.material = Instantiate(legacyMaterial);
-
+        if (mat != null)
+            rend.material = Instantiate(mat);
         float elapsed = 0f;
-        while (elapsed < legacyDuration)
+        while (elapsed < duration)
         {
-            float t = elapsed / legacyDuration;
+            float t = elapsed / duration;
             float diameter = Mathf.Lerp(startDiameter, targetDiameter, t);
             sphere.transform.localScale = Vector3.one * diameter;
             elapsed += Time.deltaTime;
             yield return null;
         }
-
         Destroy(gameObject);
     }
 }
